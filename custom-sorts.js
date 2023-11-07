b6300677bde924da9f78c7007da1106983d671b4
Michael <michael_lacy95@icloud.com>
2023-11-06 23:24:31 -0600

    finished

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,28 +1,51 @@
 function ageSort(users) {
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].age > users[i+1].age) {
-      [users[i], users[i+1]] = [users[i+1], users[i]]
-      return users
-    }
-  }
+  return users.sort((a, b) => {
+    return a.age - b.age
+  });
 }
 
 function oddEvenSort(arr) {
-  const odd = arr.filter(el => {
-    el % 2 !== 0
-  })
-  arr.sort(compare)
+  const odd = arr.filter(el => el % 2 === 1).sort(compNums)
+  const even = arr.filter(el => el % 2 === 0).sort(compNums)
+
+  return [...odd, ...even]
 
 }
-function compare(a, b) {
+function compNums(a, b){
   return a - b
 }
-function validAnagrams(s, t) {
-  // Your code here
-}
+
+let validAnagrams = (s, t) => s.split('').sort().join('') === t.split('').sort().join('')
+
+
 
 function reverseBaseSort(arr) {
-  console.log(arr.sort())
+  let stringSorted = arr.map(el => el.toString()).sort(compNums)
+  console.log(stringSorted)
+  let pointer = 0
+
+  while (pointer < stringSorted.length) {
+    // Do not move this console.log
+
+    // Find the index of the minimum value in the unsorted half
+    let maxIndex = pointer
+    for (let i = pointer; i < stringSorted.length; i++) {
+      if (stringSorted[maxIndex].length < stringSorted[i].length) {
+        maxIndex = i
+      }
+    }
+    //Save the min value
+    let maxValue = stringSorted[maxIndex]
+    // Shift every unsorted value to the left of the min value to the right by 1
+    for (let j = maxIndex; j > pointer; j--) {
+      stringSorted[j] = stringSorted[j-1]
+    }
+    // Put the min value at the divider
+    stringSorted[pointer] = maxValue
+    // Increment the divider and repeat
+    pointer ++
+  }
+  return stringSorted.map(el => parseInt(el))
 }
 
 function frequencySort(arr) {
