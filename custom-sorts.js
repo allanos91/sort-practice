cf7b24e0f9b60ae1776d17de8d6152701d2fd4aa
Alexander <allanosinhb@gmail.com>
2023-11-06 21:16:07 -0800

    1 and last

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,21 +1,52 @@
 function ageSort(users) {
-  // Your code here 
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].age > users[i+1].age) {
+      [users[i], users[i+1]] = [users[i+1], users[i]]
+      return users
+    }
+  }
 }
 
 function oddEvenSort(arr) {
-  // Your code here 
-}
+  const odd = arr.filter(el => {
+    el % 2 !== 0
+  })
+  arr.sort(compare)
 
+}
+function compare(a, b) {
+  return a - b
+}
 function validAnagrams(s, t) {
-  // Your code here 
+  // Your code here
 }
 
 function reverseBaseSort(arr) {
-  // Your code here 
+  console.log(arr.sort())
 }
 
 function frequencySort(arr) {
-  // Your code here 
+  let newObj = {}
+  arr.forEach(el => {
+    if (!newObj[el]) {
+      newObj[el] = 0
+    }
+    newObj[el] += 1
+  })
+  return arr.sort((a,b) => {
+    if (newObj[a] < newObj[b]) {
+      return -1
+    }
+    else if (newObj[a] > newObj[b]) {
+      return 1
+    }
+    else if (newObj[a] === newObj[b]) {
+      if (a > b) {
+        return -1
+      }
+    }
+  })
+
 }
 
 module.exports = [
